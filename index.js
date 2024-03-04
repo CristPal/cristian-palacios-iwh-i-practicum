cdca7639e8ff45b5ec138e2e4c70bd776c14f39f
cristpal <palacioscristian0708@gmail.com>
2024-03-04 11:43:59 -0400

    Controllers for home and update-cobj added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config()
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,19 +9,53 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_TOKEN
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
 
+app.get('/', async (req, res) => {
+
+    const getUrl = 'https://api.hubapi.com/crm/v3/objects/2-25156322?properties=name,role'
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(getUrl, { headers });
+        const data = resp.data.results;
+        res.render('home', { title: 'Fighter Jet Data | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+
+})
+
+
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
 
+app.get('/update-cobj', async (req, res) => {
+
+    res.render('updates');      
+    
+
+})
+
+
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+
+app.post('/update-cobj', async (req,res) => {
+
+    
+
+})
+
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -65,7 +100,6 @@ app.post('/update', async (req, res) => {
 
 });
 */
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
